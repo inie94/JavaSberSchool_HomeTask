a3202491f636896966803bffe6a6afe2d5dfede8
inie94 <ananian94@gmail.com>
2021-08-18 00:07:35 +0300

    Initial commit

---
+++ javasberschool/src/homework/lesson1/BubbleSorting.java
@@ -0,0 +1,40 @@
+package homework.lesson1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleSorting {
+    public static void main(String[] args) {
+//        List<Integer> list = Arrays.asList(0, 7, 3, 9, 5, 2, 6, 1, 8, 4);
+        List<String> list = Arrays.asList("F", "B", "J", "D", "H", "E", "A", "G", "I", "C");
+
+        printList(list);
+
+        list = bubbleSort(list);
+
+        printList(list);
+    }
+
+    public static <T> void printList(List<T> list) {
+        for (T item : list) {
+            System.out.print(item);
+        }
+        System.out.println();
+    }
+
+    public static <T extends Comparable<T>> List<T> bubbleSort(List<T> list) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int j = 0; j < list.size() - 1; j++) {
+                T temp = list.get(j);
+                if (list.get(j + 1).compareTo(temp) < 0) {
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                    isSorted = false;
+                }
+            }
+        }
+        return list;
+    }
+}
