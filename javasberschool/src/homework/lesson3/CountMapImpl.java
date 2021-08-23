a50be68e2b1fbcf32bb795f3b89fbb6debde8610
inie94 <ananian94@gmail.com>
2021-08-23 21:08:18 +0300

    Custom map complete.

---
+++ javasberschool/src/homework/lesson3/CountMapImpl.java
@@ -0,0 +1,55 @@
+package homework.lesson3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountMapImpl<K> implements CountMap<K>{
+    private Map<K, Integer> map = new HashMap<>();
+
+    @Override
+    public void add(K k) {
+        Integer count;
+        if((count = map.get(k)) == null) {
+            count = 0;
+        }
+        map.put(k, ++count);
+    }
+
+    @Override
+    public int getCount(K k) {
+        return map.get(k);
+    }
+
+    @Override
+    public int remove(K k) {
+        return map.remove(k);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public void addAll(CountMap source) {
+        for (Object key: source.toMap().keySet()) {
+            Integer count;
+            if((count = map.get(key)) == null) {
+                count = 0;
+            }
+            map.put((K) key, source.getCount(key) + count);
+        }
+    }
+
+    @Override
+    public Map toMap() {
+        return map;
+    }
+
+    @Override
+    public void toMap(Map destination) {
+        for (K key: map.keySet()) {
+            destination.put(key, map.get(key));
+        }
+    }
+}
---
+++ javasberschool/src/homework/lesson3/Solution.java
@@ -0,0 +1,19 @@
+package homework.lesson3;
+
+public class Solution {
+    public static void main(String[] args) {
+        CountMap<Integer> map = new CountMapImpl<>();
+
+        map.add(10);
+        map.add(10);
+        map.add(5);
+        map.add(6);
+        map.add(5);
+        map.add(10);
+
+        for (Object value:
+             map.toMap().keySet()) {
+            System.out.println(value + " " + map.getCount((Integer) value));
+        }
+    }
+}
