76bb4971f27ed316388723bd05912d536f718ec4
inie94 <ananian94@gmail.com>
2021-08-31 20:20:22 +0300

    Reflection hw tasks 5 is completed.

--- javasberschool/src/homework/lesson5/reflectionhw/task5/Solution.java
+++ javasberschool/src/homework/lesson5/reflectionhw/task5/Solution.java
@@ -12,10 +12,15 @@ public class Solution {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String request;
+            System.out.println("For close this app write \"exit\".");
+            System.out.print("Write value for calculated: ");
             while (true) {
                 if (reader.ready()) {
                     while (!(request = reader.readLine()).equals("exit")) {
-                        System.out.println(calculator.calc(Integer.parseInt(request)));
+                        System.out.println("Answer is: " + calculator.calc(Integer.parseInt(request)));
+
+                        System.out.println("For close this app write \"exit\".");
+                        System.out.print("Write value for calculated: ");
                     }
                     System.out.println("Goodbye!");
                     break;
