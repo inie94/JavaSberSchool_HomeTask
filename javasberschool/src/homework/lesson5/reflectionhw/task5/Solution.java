237eeddda5d18789c227712a5c13f96227beedb4
inie94 <ananian94@gmail.com>
2021-08-31 20:12:02 +0300

    Reflection hw tasks 5 is completed.

--- javasberschool/src/homework/lesson5/reflectionhw/task5/Solution.java
+++ javasberschool/src/homework/lesson5/reflectionhw/task5/Solution.java
@@ -1,6 +1,29 @@
 package homework.lesson5.reflectionhw.task5;
 
+import homework.lesson5.reflectionhw.task1.Calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) {
+        Calculator calculator = new CachingCalculatorProxy();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String request;
+            while (true) {
+                if (reader.ready()) {
+                    while (!(request = reader.readLine()).equals("exit")) {
+                        System.out.println(calculator.calc(Integer.parseInt(request)));
+                    }
+                    System.out.println("Goodbye!");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
