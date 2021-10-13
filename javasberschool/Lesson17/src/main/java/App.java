00b81bfeac2e69375cc8514bc5332f0275b9ccbc
inie94 <ananian94@gmail.com>
2021-10-13 21:16:22 +0300

    Lesson 17 complete.

--- javasberschool/Lesson17/src/main/java/App.java
+++ javasberschool/Lesson17/src/main/java/App.java
@@ -1,4 +1,3 @@
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.inie.lesson17.AnimalService;
@@ -7,12 +6,9 @@ import ru.inie.lesson17.Config;
 
 public class App {
 
-    @Autowired
-    private static AnimalService animalService;
-
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        animalService = context.getBean(AnimalService.class);
+        AnimalService animalService = context.getBean(AnimalService.class);
         animalService.voice();
     }
 
